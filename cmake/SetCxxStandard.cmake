57cae703b9a35659ee10d2d42460f31651d5fdb5
spoonless <dagaydevel@free.fr>
2015-02-22 01:09:47 +0100

    adding cmake macro set_cxx_standard to activate C++11 or 14

---
+++ cmake/SetCxxStandard.cmake
@@ -0,0 +1,13 @@
+# Code snippet inspired from http://www.guyrutenberg.com/2014/01/05/enabling-c11-c0x-in-cmake/
+
+include(CheckCXXCompilerFlag)
+
+macro( set_cxx_standard VERSION )
+  check_cxx_compiler_flag("-std=c++${VERSION}" "COMPILER_SUPPORTS_CXX_${VERSION}")
+  if(${COMPILER_SUPPORTS_CXX_${VERSION}})
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++${VERSION}")
+  else(VERSION NOT STREQUAL "98")
+    message(SEND_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++${VERSION} support.")
+  endif()
+endmacro()
+

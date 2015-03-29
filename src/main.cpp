62dc9175a9f6c7072c3df71c48fa5389463a4913
David Gayerie <dagaydevel@free.fr>
2015-03-29 14:49:45 +0200

    g3log integration (with mingw cross compilation patch)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
+#include "g2log.hpp"
+#include "g2logworker.hpp"
 
 int main (int argc, char **argv)
 {
+    auto logworker = g2::LogWorker::createWithDefaultLogger("main", ".");
+    g2::initializeLogging(logworker.worker.get());
+
+    LOG(INFO)<< "Application starting";
+
+    LOG(INFO)<< "Application ending";
+
     return 0;
 }
--- src/sample.cpp
+++ src/sample.cpp
@@ -1,5 +1,7 @@
+#include "g2log.hpp"
 
 int add(int x, int y)
 {
+    LOG(DEBUG) << "adding " << x << " to " << y;
     return x + y;
 }
